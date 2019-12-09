c71f99969d30e44f2af6cced767b47dadddaf51a
Administrator <Administrator@USER-8E3A4G7ST9>
2019-12-09 11:15:58 +0800

    ..

--- src/main/java/com/bigdata/springboot_1/config/PageHelperConfig.java
+++ src/main/java/com/bigdata/springboot_1/config/PageHelperConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.github.pagehelper.PageHelper;
 @Configuration
-//@Configuration启动容器+@Bean注册Bean
+//@Configuration启动容器+@Bean注册Bean 
 public class PageHelperConfig {
 //	@Bean下管理bean的生命周期
 //	@Bean标注在方法上(返回某个实例的方法)，等价于spring的xml配置文件中的<bean>，作用为：注册bean对象 
